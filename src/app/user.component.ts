511155b07af97a721f79b62793622075d85e8254
kaixuan <kaixuant@yahoo.com>
2023-02-14 14:05:12 +0800

    event binding and property binding

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
-
+/* As you can see the input is event binding and the value is properties binding,
+which the syntax is writing in this way */
 @Component({
   selector: 'app-user',
-  template: "<p>Hello!</p><p>I'm user component</p>",
+  template: ` <input type="text" (input)="onUserInput($event)" [value]="name" />
+    <p>Hello!</p>
+    <p>I'm {{ name }}</p>`,
 })
-export class UserComponent {}
+export class UserComponent {
+  name = 'kaixuan';
+  onUserInput(event: any) {
+    this.name = event.target.value;
+  }
+}
